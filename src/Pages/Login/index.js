b5925e11e87da337d718b8e5ccd039b713f6248e
voncardoso <vonharrison26@gmail.com>
2022-10-10 16:47:51 -0300

    visibilade da senha

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -5,12 +5,18 @@ import { Container } from "./style";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import backgroungLogin from "../../assets/imagem-festa-tela-login.png";
+import { Eye, EyeSlash } from "phosphor-react";
 
 export function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [login, setLogin] = useState(null);
   const [error, setError] = useState(null);
+  const [isTypePassword, setIsTypePassword] = useState(false);
+  let textPassword = "password";
+  if (isTypePassword === true) {
+    textPassword = "text";
+  }
 
   const navigate = useNavigate();
 
@@ -54,10 +60,27 @@ export function Login() {
         <div>
           <label htmlFor="">Senha</label>
           <input
-            type="password"
+            type={textPassword}
             value={password}
             onChange={(event) => setPassword(event.target.value)}
           />
+          {isTypePassword ? (
+            <Eye
+              className="iconViewPasswor"
+              size={24}
+              onClick={() => {
+                setIsTypePassword(false);
+              }}
+            />
+          ) : (
+            <EyeSlash
+              size={24}
+              className="iconViewPasswor"
+              onClick={() => {
+                setIsTypePassword(true);
+              }}
+            />
+          )}
         </div>
         {login ? (
           <button disabled style={{ background: "#70B6F2" }}>
